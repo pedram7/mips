f057454076dc0974969bb7711bc4994d94a9cf95
Pedram <34384623+pedram7@users.noreply.github.com>
2019-08-04 11:32:48 +0430

    Delete IF-ID Register.v

--- IF-ID Register.v
+++
@@ -1,9 +0,0 @@
-module IFIDreg(input clock , input [31:0]  instruction ,PcP4, output reg[31:0]IFIDoutInstruction ,IFIDoutPCP4);
-  
-  always@ (posedge clock)
-  begin
-   IFIDoutPCP4 = PcP4;
-   IFIDoutInstruction = instruction;
-  end
-  
-endmodule
