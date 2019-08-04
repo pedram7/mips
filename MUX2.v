5ca01c907b752354628989cda1812c128487134a
Pedram <34384623+pedram7@users.noreply.github.com>
2019-08-04 11:33:01 +0430

    Delete MUX2.v

--- MUX2.v
+++
@@ -1,7 +0,0 @@
-module MUX2(input[31:0] a , b ,input s , output[31:0] m2out);
-  assign m2out = s?b:a;
-endmodule
-
-module fivebitMUX2(input[4:0] a , b ,input s , output[4:0] m2out);
-  assign m2out = s?b:a;
-endmodule
